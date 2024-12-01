fb97c61dcdbc587b18629655cb1044b2aae6f962
JBeraldo <joaoangelo409@gmail.com>
2024-12-01 17:07:51 -0300

    INFRA: change infra settings

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,14 @@ use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/ping',function (){
+    return 'pong';
+})->middleware('api');
+
+Route::get('/info',function (){
+    return phpinfo();
+})->middleware('api');
+
 // Usuários
 Route::get('/me', [UserController::class, 'getMe'])->middleware(['api', 'auth']);
 
